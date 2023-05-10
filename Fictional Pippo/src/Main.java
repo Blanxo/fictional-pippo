43a149772462001332d37240acd8308cf31f8de1
Francesco Bianco <francescobianco00@gmail.com>
2023-05-10 08:41:37 +0200

    Spostato i Pippi su un branch

--- Fictional Pippo/src/Main.java
+++ Fictional Pippo/src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Ciao Pippo!");
-        System.out.println("Ciao Pluto!");
+        Pipponia.stampa();
     }
 }
---
+++ Fictional Pippo/src/Pipponia.java
@@ -0,0 +1,7 @@
+public class Pipponia {
+    public static void stampa() {
+        System.out.println("Ciao Pippo!");
+        System.out.println("Ciao Pluto!");
+    }
+
+}
